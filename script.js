1a4e51357065089f9be8f3ec6ea02139aee308a9
Loucasse <takazyt@gmail.com>
2016-02-21 16:28:42 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Barre d'outils RealityGaming
+// @namespace    https://realitygaming.fr/*
+// @version      1
+// @description  Ce script ajoute une barre d'outils sur RealityGaming
+// @author       Loucasse
+// @match        https://realitygaming.fr/*
+// @grant        none
+// ==/UserScript==
+
+// Script codé par loucasse, tous droits réservés //
+
+$(document).ready(function(){
+    $.getScript('https://cdn.rawgit.com/LoucasseRG/politesse/master/script.js');
+});
